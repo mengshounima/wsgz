3b37e85e3190ac1cb442edcb16b0ebc2ae81ab22
李 燕琴 <1195407656@qq.com>
2017-11-08 23:34:55 +0800

    扩大按钮位置

--- FiveWaterWork/UIBarButtonItem+Extension.m
+++ FiveWaterWork/UIBarButtonItem+Extension.m
@@ -12,11 +12,11 @@
 + (UIBarButtonItem *)itemWithImageName:(NSString *)imageName highImageName:(NSString *)highImageName target:(id)target action:(SEL)action
 {
     UIButton *button = [[UIButton alloc] init];
-    [button setBackgroundImage:[UIImage imageWithName:imageName] forState:UIControlStateNormal];
-    [button setBackgroundImage:[UIImage imageWithName:highImageName] forState:UIControlStateHighlighted];
+    [button setImage:[UIImage imageWithName:imageName] forState:UIControlStateNormal];
+    [button setImage:[UIImage imageWithName:highImageName] forState:UIControlStateHighlighted];
     
     // 设置按钮的尺寸为背景图片的尺寸
-    button.size = button.currentBackgroundImage.size;
+    button.size = CGSizeMake(30, 30);
     
     // 监听按钮点击
     [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
